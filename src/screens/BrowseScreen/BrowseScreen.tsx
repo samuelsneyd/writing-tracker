a9161011e371f457c04d290044aad7b78fb9cbdc
Samuel <samuelsneyd@gmail.com>
2023-04-28 23:09:13 +1200

    Init BrowseScreen

---
+++ src/screens/BrowseScreen/BrowseScreen.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const BrowseScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Browse</Text>
+    </View>
+  );
+};
+
+export default BrowseScreen;
