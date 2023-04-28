ee862ea896fd7b42d381571888b57d565addbaf7
Samuel <samuelsneyd@gmail.com>
2023-04-28 23:06:53 +1200

    Init ProjectsScreen

---
+++ src/screens/ProjectsScreen/ProjectsScreen.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const ProjectsScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Projects</Text>
+    </View>
+  );
+};
+
+export default ProjectsScreen;
