40f5639d0ba5b1aa548b6dcebb1a4e0834da52aa
Samuel <samuelsneyd@gmail.com>
2023-04-30 15:32:56 +1200

    Update daily quote on local timezone

--- src/hooks/useDailyQuote/useDailyQuote.tsx
+++ src/hooks/useDailyQuote/useDailyQuote.tsx
@@ -1,13 +1,20 @@
+import * as React from 'react';
 import { useState, useEffect } from 'react';
 import type { Quote } from '../../types/types';
 import quotes from './quotes';
 
+/**
+ * Returns the daily quote. The quote updates daily, in order,
+ * based on the device's local timezone.
+ */
 const useDailyQuote = (): Quote => {
   const [dailyQuote, setDailyQuote] = useState<Quote>({ quote: '', author: '' });
 
   useEffect(() => {
     const now = new Date();
-    const millisecondsSinceEpoch = now.getTime();
+    // Convert to local timezone
+    const timezoneOffsetMilliseconds = now.getTimezoneOffset() * 60 * 1000;
+    const millisecondsSinceEpoch = now.getTime() + timezoneOffsetMilliseconds;
     const daysSinceEpoch = Math.floor(millisecondsSinceEpoch / (1000 * 60 * 60 * 24));
     const dailyQuoteIndex = daysSinceEpoch % quotes.length;
 
