6605d176f7a4a09ce02ec240cf64b2ae71e28a36
Samuel <samuelsneyd@gmail.com>
2023-05-08 15:34:08 +1200

    Add more daily quotes

--- src/hooks/useDailyQuote/quotes.ts
+++ src/hooks/useDailyQuote/quotes.ts
@@ -29,6 +29,122 @@ const quotes: Quote[] = [
     quote: 'The English language is an arsenal of weapons. If you are going to brandish them without checking to see whether or not they are loaded, you must expect to have them explode in your face from time to time.',
     author: 'Stephen Fry',
   },
+  {
+    quote: 'A professional writer is an amateur who didn\'t quit.',
+    author: 'Richard Bach',
+  },
+  {
+    quote: 'I only write when I\'m inspired, so I see to it that I\'m inspired every morning at nine o\'clock.',
+    author: 'Peter De Vries',
+  },
+  {
+    quote: 'Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work.',
+    author: 'Steven King',
+  },
+  {
+    quote: 'Just write every day of your life. Read intensely. Then see what happens. Most of my friends who are put on that diet have very pleasant careers.',
+    author: 'Ray Bradbury',
+  },
+  {
+    quote: 'Close the door. Write with no one looking over your shoulder. Don\'t try to figure out what other people want to hear from you; figure out what you have to say. It\'s the one and only thing you have to offer.',
+    author: 'Barbara Kingsolver',
+  },
+  {
+    quote: 'If my doctor told me I had only six minutes to live, I wouldn\'t brood. I\'d type a little faster.',
+    author: 'Isaac Asimov',
+  },
+  {
+    quote: 'Every writer I know has trouble writing.',
+    author: 'Joseph Heller',
+  },
+  {
+    quote: 'Good writing is remembering detail. Most people want to forget. Don\'t forget things that were painful or embarrassing or silly. Turn them into a story that tells the truth.',
+    author: 'Paula Danziger',
+  },
+  {
+    quote: 'There is no rule on how to write. Sometimes it comes easily and perfectly: sometimes it\'s like drilling rock and then blasting it out with charges.',
+    author: 'Ernest Hemingway',
+  },
+  {
+    quote: 'The scariest moment is always just before you start. After that, things can only get better.',
+    author: 'Stephen King',
+  },
+  {
+    quote: 'And by the way, everything in life is writable about if you have the outgoing guts to do it, and the imagination to improvise. The worst enemy to creativity is self-doubt.',
+    author: 'Sylvia Plath',
+  },
+  {
+    quote: 'The best time to plan a book is while you\'re doing the dishes.',
+    author: 'Agatha Christie',
+  },
+  {
+    quote: 'There are three rules for writing the novel. Unfortunately, no one knows what they are.',
+    author: 'W. Somerset Maugham',
+  },
+  {
+    quote: '[As a writer] you have to have the three D\'s: drive, discipline and desire. If you\'re missing any one of those three, you can have all the talent in the world, but it\'s going to be really hard to get anything done.',
+    author: 'Nora Roberts',
+  },
+  {
+    quote: 'I try to write a certain amount each day, five days a week. A rule sometimes broken is better than no rule.',
+    author: 'Herman Wouk',
+  },
+  {
+    quote: 'We will open the book. Its pages are blank. We are going to put words on them ourselves. The book is called Opportunity and its first chapter is New Year\'s Day.',
+    author: 'Edith Lovejoy Pierce',
+  },
+  {
+    quote: 'There\'s no better teacher for writing than reading… Get a library card. That\'s the best investment.',
+    author: 'Alisa Valdes',
+  },
+  {
+    quote: 'Beginning a novel is always hard. It feels like going nowhere. I always have to write at least 100 pages that go into the trashcan before it finally begins to work. It\'s discouraging, but necessary to write those pages. I try to consider them pages -100 to zero of the novel.',
+    author: 'Barbara Kingsolver',
+  },
+  {
+    quote: 'You have to follow your own voice. You have to be yourself when you write. In effect, you have to announce, ‘This is me, this is what I stand for, this is what you get when you read me. I\'m doing the best I can—buy me or not—but this is who I am as a writer.',
+    author: 'David Morrell',
+  },
+  {
+    quote: 'A writer is a person for whom writing is more difficult than it is for other people.',
+    author: 'Thomas Mann',
+  },
+  {
+    quote: 'The miraculous connection between writing and the immune system results from cracking through inhibition. It seems that when we don\'t speak the truth of our experience, we inhibit our emotions, and that inhibits our immune function. Keeping secrets and maintaining denial require physical energy, energy our bodies could use in healthier ways were it available.',
+    author: 'Peggy Tabor Millin',
+  },
+  {
+    quote: 'If you write one story, it may be bad; if you write a hundred, you have the odds in your favor.',
+    author: 'Edgar Rice Burroughs',
+  },
+  {
+    quote: 'Being a good writer is 3% talent, 97% not being distracted by the Internet.',
+    author: 'Anon',
+  },
+  {
+    quote: 'If you wait for inspiration to write you\'re not a writer, you\'re a waiter.',
+    author: 'Dan Poynter',
+  },
+  {
+    quote: 'If a story is not about the hearer, he will not listen. And here I make a rule - a great and interesting story is about everyone or it will not last.',
+    author: 'John Steinbeck',
+  },
+  {
+    quote: 'Words can be like X-rays if you use them properly - they\'ll go through anything. You read and you\'re pierced.',
+    author: 'Aldous Huxley, Brave New World',
+  },
+  {
+    quote: 'My first feeling was that there was no way to continue. Writing isn\'t like math; in math, two plus two always equals four no matter what your mood is like. With writing, the way you feel changes everything.',
+    author: 'Stephenie Meyer',
+  },
+  {
+    quote: 'There is probably no hell for authors in the next world - they suffer so much from critics and publishers in this one.',
+    author: 'C.N. Bovee',
+  },
+  {
+    quote: 'There is no greater threat to the critics and cynics and fear-mongers than those of us who are willing to fall because we have learned how to rise.',
+    author: 'Brené Brown',
+  },
 ];
 
 export default quotes;
