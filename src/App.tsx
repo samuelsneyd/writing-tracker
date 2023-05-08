8b413dfdc6de649b83629cb178f9ba5250d6b20a
Samuel <samuelsneyd@gmail.com>
2023-05-08 12:33:04 +1200

    Init theme context and light/dark theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import * as React from 'react';
+import { useState } from 'react';
 import * as eva from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import RootTabNavigator from './navigators/RootTabNavigator/RootTabNavigator';
+import { ThemeContext } from './themes/theme-context';
 import { cyanTheme } from './themes';
 
 const App = () => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  const toggleTheme = () => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+  };
+
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={{ ...eva.light, ...cyanTheme }}>
-        <NavigationContainer>
-          <RootTabNavigator />
-        </NavigationContainer>
-      </ApplicationProvider>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <ApplicationProvider {...eva} theme={{ ...eva[theme], ...cyanTheme }}>
+          <NavigationContainer>
+            <RootTabNavigator />
+          </NavigationContainer>
+        </ApplicationProvider>
+      </ThemeContext.Provider>
     </>
   );
 };
--- src/screens/ThemesScreen/ThemesScreen.tsx
+++ src/screens/ThemesScreen/ThemesScreen.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
+import { useContext } from 'react';
 import { SafeAreaView } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { MoreStackParamList } from '../../types/types';
-import { Divider, Layout, TopNavigation, Text } from '@ui-kitten/components';
+import { Divider, Layout, TopNavigation, Text, Button } from '@ui-kitten/components';
 import useBackNavigation from '../../hooks/useBackNavigation/useBackNavigation';
+import { ThemeContext } from '../../themes/theme-context';
 
 type Props = NativeStackScreenProps<MoreStackParamList, 'Themes'>
 
 const ThemesScreen = ({ navigation }: Props) => {
   const { BackAction } = useBackNavigation(navigation);
+  const themeContext = useContext(ThemeContext);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -16,6 +19,7 @@ const ThemesScreen = ({ navigation }: Props) => {
       <Divider />
       <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text category="h1">Themes</Text>
+        <Button onPress={themeContext.toggleTheme}>Toggle light/dark mode</Button>
       </Layout>
     </SafeAreaView>
   );
