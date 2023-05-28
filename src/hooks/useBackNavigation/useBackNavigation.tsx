b6df49e55c7743012b3798337382672488e28f63
Samuel <samuelsneyd@gmail.com>
2023-05-28 20:23:07 +1200

    Set correct back navigation types and add docs

--- src/hooks/useBackNavigation/useBackNavigation.tsx
+++ src/hooks/useBackNavigation/useBackNavigation.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
 import { Icon, IconProps, TopNavigationAction } from '@ui-kitten/components';
+import { NavigationProp, ParamListBase } from '@react-navigation/native';
 
 /**
  * Returns a BackAction component function that navigates to the previous stack screen
  * on press. For use with UI Kitten and React Navigation's top stack navigator.
  * @param navigation React Navigation prop
  */
-const useBackNavigation = (navigation: any) => {
+const useBackNavigation = (navigation: NavigationProp<ParamListBase>) => {
   const BackIcon = (props: IconProps) => (
     <Icon {...props} name="arrow-back" />
   );
 
+  /**
+   * Returns a TopNavigationAction component with a back icon
+   * that returns to the previous stack screen on press.
+   */
   const BackAction = () => (
     <TopNavigationAction icon={BackIcon} onPress={() => navigation.goBack()} />
   );
