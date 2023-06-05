8828545508449fe45d18a3f09e34d8e43ef5effd
Samuel <samuelsneyd@gmail.com>
2023-06-05 20:11:27 +1200

    Add daily word toggles and inputs to new project form.

--- src/screens/ProjectNewScreen/ProjectNewScreen.tsx
+++ src/screens/ProjectNewScreen/ProjectNewScreen.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import type { ProjectsStackParamList } from '../../types/types';
 import { Project, ProjectStatus, ProjectType } from '../../models';
-import { Divider, IndexPath, Input, Layout, Select, SelectItem, TopNavigation } from '@ui-kitten/components';
+import {
+  Divider,
+  IndexPath,
+  Input,
+  Layout,
+  Select,
+  SelectItem,
+  Text,
+  Toggle,
+  TopNavigation,
+} from '@ui-kitten/components';
 import { capitalCase } from 'change-case';
 import useBackNavigation from '../../hooks/useBackNavigation/useBackNavigation';
 
@@ -25,11 +35,28 @@ const ProjectNewScreen = ({ navigation }: Props): React.ReactElement => {
   const [selectedTypeIndex, setSelectedTypeIndex] = React.useState<IndexPath>(new IndexPath(0));
   const [status, setStatus] = React.useState<ProjectStatus>(ProjectStatus.IN_PROGRESS);
   const [selectedStatusIndex, setSelectedStatusIndex] = React.useState<IndexPath>(new IndexPath(0));
+  const [monTarget, setMonTarget] = React.useState<string>('');
+  const [monToggleChecked, setMonToggleChecked] = React.useState<boolean>(true);
+  const [tueTarget, setTueTarget] = React.useState<string>('');
+  const [tueToggleChecked, setTueToggleChecked] = React.useState<boolean>(true);
+  const [wedTarget, setWedTarget] = React.useState<string>('');
+  const [wedToggleChecked, setWedToggleChecked] = React.useState<boolean>(true);
+  const [thuTarget, setThuTarget] = React.useState<string>('');
+  const [thuToggleChecked, setThuToggleChecked] = React.useState<boolean>(true);
+  const [friTarget, setFriTarget] = React.useState<string>('');
+  const [friToggleChecked, setFriToggleChecked] = React.useState<boolean>(true);
+  const [satTarget, setSatTarget] = React.useState<string>('');
+  const [satToggleChecked, setSatToggleChecked] = React.useState<boolean>(false);
+  const [sunTarget, setSunTarget] = React.useState<string>('');
+  const [sunToggleChecked, setSunToggleChecked] = React.useState<boolean>(false);
+
   const [project, setProject] = React.useState<Project>(new Project({
     name: '',
     description: '',
-    projectType: ProjectType.BOOK,
+    type: ProjectType.BOOK,
     status: ProjectStatus.IN_PROGRESS,
+    initialWords: 0,
+    overallWordTarget: 0,
     wordTarget: {
       mon: { enabled: true, words: 0 },
       tue: { enabled: true, words: 0 },
@@ -39,7 +66,7 @@ const ProjectNewScreen = ({ navigation }: Props): React.ReactElement => {
       sat: { enabled: false, words: 0 },
       sun: { enabled: false, words: 0 },
     },
-    wordsPerPage: 300
+    wordsPerPage: 300,
   }));
 
   const { BackAction } = useBackNavigation(navigation);
@@ -87,9 +114,145 @@ const ProjectNewScreen = ({ navigation }: Props): React.ReactElement => {
         >
           {PROJECT_STATUS_DATA.map(status => renderOption(status.display))}
         </Select>
+        <Text appearance="hint">Daily targets</Text>
+        <Layout style={styles.dailyTargetRow}>
+          <Toggle
+            style={styles.dailyTargetRowItem}
+            checked={monToggleChecked}
+            onChange={() => {
+              monToggleChecked && setMonTarget('');
+              setMonToggleChecked(!monToggleChecked);
+            }}
+          >Monday</Toggle>
+          <Input
+            style={styles.dailyTargetRowItem}
+            placeholder="0"
+            value={monTarget}
+            onChangeText={nextValue => setMonTarget(nextValue.replace(/\D/g, ''))}
+            disabled={!monToggleChecked}
+            keyboardType="number-pad"
+          ></Input>
+        </Layout>
+        <Layout style={styles.dailyTargetRow}>
+          <Toggle
+            style={styles.dailyTargetRowItem}
+            checked={tueToggleChecked}
+            onChange={() => {
+              tueToggleChecked && setTueTarget('');
+              setTueToggleChecked(!tueToggleChecked);
+            }}
+          >Tuesday</Toggle>
+          <Input
+            style={styles.dailyTargetRowItem}
+            placeholder="0"
+            value={tueTarget}
+            onChangeText={nextValue => setTueTarget(nextValue.replace(/\D/g, ''))}
+            disabled={!tueToggleChecked}
+            keyboardType="number-pad"
+          ></Input>
+        </Layout>
+        <Layout style={styles.dailyTargetRow}>
+          <Toggle
+            style={styles.dailyTargetRowItem}
+            checked={wedToggleChecked}
+            onChange={() => {
+              wedToggleChecked && setWedTarget('');
+              setWedToggleChecked(!wedToggleChecked);
+            }}
+          >Wednesday</Toggle>
+          <Input
+            style={styles.dailyTargetRowItem}
+            placeholder="0"
+            value={wedTarget}
+            onChangeText={nextValue => setWedTarget(nextValue.replace(/\D/g, ''))}
+            disabled={!wedToggleChecked}
+            keyboardType="number-pad"
+          ></Input>
+        </Layout>
+        <Layout style={styles.dailyTargetRow}>
+          <Toggle
+            style={styles.dailyTargetRowItem}
+            checked={thuToggleChecked}
+            onChange={() => {
+              thuToggleChecked && setThuTarget('');
+              setThuToggleChecked(!thuToggleChecked);
+            }}
+          >Thursday</Toggle>
+          <Input
+            style={styles.dailyTargetRowItem}
+            placeholder="0"
+            value={thuTarget}
+            onChangeText={nextValue => setThuTarget(nextValue.replace(/\D/g, ''))}
+            disabled={!thuToggleChecked}
+            keyboardType="number-pad"
+          ></Input>
+        </Layout>
+        <Layout style={styles.dailyTargetRow}>
+          <Toggle
+            style={styles.dailyTargetRowItem}
+            checked={friToggleChecked}
+            onChange={() => {
+              friToggleChecked && setFriTarget('');
+              setFriToggleChecked(!friToggleChecked);
+            }}
+          >Friday</Toggle>
+          <Input
+            style={styles.dailyTargetRowItem}
+            placeholder="0"
+            value={friTarget}
+            onChangeText={nextValue => setFriTarget(nextValue.replace(/\D/g, ''))}
+            disabled={!friToggleChecked}
+            keyboardType="number-pad"
+          ></Input>
+        </Layout>
+        <Layout style={styles.dailyTargetRow}>
+          <Toggle
+            style={styles.dailyTargetRowItem}
+            checked={satToggleChecked}
+            onChange={() => {
+              satToggleChecked && setSatTarget('');
+              setSatToggleChecked(!satToggleChecked);
+            }}
+          >Saturday</Toggle>
+          <Input
+            style={styles.dailyTargetRowItem}
+            placeholder="0"
+            value={satTarget}
+            onChangeText={nextValue => setSatTarget(nextValue.replace(/\D/g, ''))}
+            disabled={!satToggleChecked}
+            keyboardType="number-pad"
+          ></Input>
+        </Layout>
+        <Layout style={styles.dailyTargetRow}>
+          <Toggle
+            style={styles.dailyTargetRowItem}
+            checked={sunToggleChecked}
+            onChange={() => {
+              sunToggleChecked && setSunTarget('');
+              setSunToggleChecked(!sunToggleChecked);
+            }}
+          >Sunday</Toggle>
+          <Input
+            style={styles.dailyTargetRowItem}
+            placeholder="0"
+            value={sunTarget}
+            onChangeText={nextValue => setSunTarget(nextValue.replace(/\D/g, ''))}
+            disabled={!sunToggleChecked}
+            keyboardType="number-pad"
+          ></Input>
+        </Layout>
       </Layout>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  dailyTargetRow: {
+    flexDirection: 'row',
+  },
+  dailyTargetRowItem: {
+    flex: 1,
+  },
+});
 
 export default ProjectNewScreen;
