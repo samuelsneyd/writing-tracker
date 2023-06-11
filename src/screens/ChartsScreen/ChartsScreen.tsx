1a98b9ffebe5821bc979331ec52a63d5d65f3475
Samuel <samuelsneyd@gmail.com>
2023-06-11 21:29:26 +1200

    Add TotalWordsByProjectChart to charts screen

--- src/screens/ChartsScreen/ChartsScreen.tsx
+++ src/screens/ChartsScreen/ChartsScreen.tsx
@@ -4,7 +4,8 @@ import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useIsFocused } from '@react-navigation/native';
 import { DataStore, Predicates } from 'aws-amplify';
 import TotalWordsByDayChart from '../../components/Charts/TotalWordsByDayChart';
-import { Session } from '../../models';
+import TotalWordsByProjectChart from '../../components/Charts/TotalWordsByProjectChart';
+import { Project, Session } from '../../models';
 import type { MoreStackParamList } from '../../types/types';
 import {
   Divider,
@@ -18,8 +19,18 @@ import { ArrowIosBackIcon } from '../../components/Icons/Icons';
 type Props = NativeStackScreenProps<MoreStackParamList, 'Charts'>
 
 const ChartsScreen = ({ navigation }: Props): React.ReactElement => {
+  const [projects, setProjects] = React.useState<Project[]>([]);
   const [sessions, setSessions] = React.useState<Session[]>([]);
   const isFocused = useIsFocused();
+
+  React.useEffect(() => {
+    const fetchProjects = async () => {
+      const foundProjects = await DataStore.query(Project, Predicates.ALL);
+      setProjects(foundProjects);
+    };
+
+    fetchProjects().then();
+  }, [isFocused]);
 
   React.useEffect(() => {
     const fetchSessions = async () => {
@@ -38,9 +49,11 @@ const ChartsScreen = ({ navigation }: Props): React.ReactElement => {
     <SafeAreaView style={styles.container}>
       <TopNavigation title="Charts" alignment="center" accessoryLeft={backAction} />
       <Divider />
-      <ScrollView style={styles.container} contentContainerStyle={styles.container}>
+      <ScrollView style={styles.container}>
         <Layout style={styles.body}>
           <Text category="h1">Charts</Text>
+          <TotalWordsByProjectChart sessions={sessions} projects={projects} />
+          <Divider style={styles.divider} />
           <TotalWordsByDayChart sessions={sessions} />
         </Layout>
       </ScrollView>
@@ -57,7 +70,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
+    paddingBottom: 64,
     gap: 10,
+  },
+  divider: {
+    paddingVertical: 10,
   },
 });
 
