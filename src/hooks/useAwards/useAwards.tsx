24702516826c10b150e2160cb16fad82164f02ac
Samuel <samuelsneyd@gmail.com>
2023-06-17 22:08:36 +1200

    Refactor useAwards hook for redux

--- src/hooks/useAwards/useAwards.tsx
+++ src/hooks/useAwards/useAwards.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import { useAppSelector } from '../../store/hooks';
 import { awards, AwardMap } from './awards';
 
-const useAward = () => {
+export type AwardSummary = {
+  allAwards: AwardMap;
+  eligibleAwards: AwardMap;
+  ineligibleAwards: AwardMap;
+};
+
+const useAwards = () => {
+  const reduxProjects = useAppSelector(state => state.projects);
+  const reduxSessions = useAppSelector(state => state.sessions);
   const [availableAwards, setAvailableAwards] = React.useState<AwardMap>();
 
   React.useEffect(() => {
     setAvailableAwards(awards);
-  }, []);
+  }, [reduxProjects, reduxSessions]);
 
   return availableAwards;
 };
 
-export default useAward;
+export default useAwards;
