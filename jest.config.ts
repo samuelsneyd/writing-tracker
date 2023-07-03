d7ab82cd3fbc18fd4c5bceb29c66fc4ebf719844
Samuel <samuelsneyd@gmail.com>
2023-07-03 10:24:48 +1000

    Add jest-setup.js and mocks

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,10 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@ui-kitten)/)',
-    'amplify/#current-cloud-backend'
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@ui-kitten|aws-amplify)/)',
+    'amplify/#current-cloud-backend',
   ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
 };
 
 export default config;
