3171f395ec14957b945a6546906c5845acfb6683
Samuel <samuelsneyd@gmail.com>
2023-07-04 14:32:47 +1000

    Add daily tasks hook and home page components

---
+++ src/hooks/useDailyTasks/useDailyTasks.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import _ from 'lodash';
+import { format, isToday } from 'date-fns';
+import { WeeklyTarget } from '../../models';
+import { SerializedProject } from '../../models/serialized';
+import { useAppSelector } from '../../store/hooks';
+import { ProjectStatus } from '../../types/API';
+
+type DailyTask = {
+  project: SerializedProject;
+  wordsToDo: number;
+  wordsCompleted: number;
+  progress: number; // Range (0-1)
+};
+
+const useDailyTasks = (): DailyTask[] => {
+  const reduxProjects = useAppSelector(state => state.projects);
+  const reduxSessions = useAppSelector(state => state.sessions);
+
+  // Group today's sessions by projects
+  const groupedSessions = _(reduxSessions)
+    .filter(session => isToday(new Date(session.date)))
+    .groupBy('projectSessionsId')
+    .value();
+
+  // Get today's day key 'mon' | 'tue' | ... | 'sun'
+  const today = new Date();
+  const dayKey = format(today, 'E').toLowerCase() as keyof WeeklyTarget;
+
+  return reduxProjects
+    // Active projects with targets today
+    .filter(project =>
+      project.status === ProjectStatus.IN_PROGRESS
+      && project.wordTarget[dayKey]?.enabled
+      && project.wordTarget[dayKey]?.words > 0,
+    )
+    // Find target and progress already achieved today
+    .map(project => ({
+      project,
+      wordsToDo: project.wordTarget[dayKey].words,
+      wordsCompleted: _.sumBy(groupedSessions[project.id] ?? [], 'words'),
+    }))
+    .map(task => ({
+      ...task,
+      progress: Math.min(task.wordsCompleted / task.wordsToDo, 1),
+    }));
+};
+
+export default useDailyTasks;
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { serializeModel } from '@aws-amplify/datastore/ssr';
 import { DataStore } from 'aws-amplify';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { getSteppedColors } from '../../hooks/useAwards/award-utils';
 import { Award, Project, Session } from '../../models';
 import { SerializedAward, SerializedProject, SerializedSession } from '../../models/serialized';
 import { awardsSet } from '../../store/awards/awardsSlice';
@@ -12,13 +13,15 @@ import { projectsSet } from '../../store/projects/projectsSlice';
 import { sessionsSet } from '../../store/sessions/sessionsSlice';
 import type { HomeStackParamList } from '../../types/types';
 import {
+  Card,
   Divider,
-  Layout,
+  Layout, ProgressBar,
   Text,
   TopNavigation,
   TopNavigationAction,
   TopNavigationActionElement,
 } from '@ui-kitten/components';
+import useDailyTasks from '../../hooks/useDailyTasks/useDailyTasks';
 import DailyQuote from '../../components/DailyQuote/DailyQuote';
 import LoginStreak from '../../components/LoginStreak/LoginStreak';
 import { MenuIcon } from '../../components/Icons/Icons';
@@ -31,6 +34,7 @@ const HomeScreen = ({ navigation }: Props): React.ReactElement => {
   const reduxAwards = useAppSelector(state => state.awards);
   const reduxProjects = useAppSelector(state => state.projects);
   const reduxSessions = useAppSelector(state => state.sessions);
+  const dailyTasks = useDailyTasks();
 
   // Prefetch projects
   React.useEffect(() => {
@@ -86,13 +90,40 @@ const HomeScreen = ({ navigation }: Props): React.ReactElement => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <TopNavigation title="Home" alignment="center" accessoryLeft={renderDrawerAction} />
-      <Divider />
-      <Layout style={styles.body}>
-        <Text category="h1">Home</Text>
-        <DailyQuote isFocused={isFocused} />
-        <LoginStreak isFocused={isFocused} />
-      </Layout>
+      <ScrollView style={styles.container} contentContainerStyle={{ flexGrow: 1 }}>
+        <TopNavigation title="Home" alignment="center" accessoryLeft={renderDrawerAction} />
+        <Divider />
+        <Layout style={styles.body}>
+          <Text category="h1">Home</Text>
+          <DailyQuote isFocused={isFocused} />
+          <LoginStreak isFocused={isFocused} />
+          <Text category="h5">Daily Tasks</Text>
+          {dailyTasks.map(task => (
+            <Card
+              key={task.project.id}
+              status="basic"
+              style={styles.card}
+              header={<Text category="s1">{task.project.title}</Text>}
+              onPress={() =>
+                navigation.getParent()?.navigate('ProjectsStackNavigator', {
+                  screen: 'Details',
+                  params: { id: task.project.id, title: task.project.title },
+                })
+              }
+            >
+              <Layout style={styles.cardLayout}>
+                <Text style={styles.text}>Write {task.wordsToDo} words</Text>
+                <Text style={styles.text}>{task.wordsCompleted}/{task.wordsToDo}</Text>
+                <ProgressBar
+                  status={getSteppedColors(task.progress)}
+                  progress={task.progress}
+                  animating={false}
+                />
+              </Layout>
+            </Card>
+          ))}
+        </Layout>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -106,6 +137,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 16,
     gap: 16,
+  },
+  card: {
+    width: '100%',
+  },
+  cardLayout: {
+    flex: 1,
+    gap: 8,
+  },
+  text: {
+    textAlign: 'left',
   },
 });
 
