1d1043772d9fe81e0b66ceb1217710645754d709
Samuel <samuelsneyd@gmail.com>
2023-07-05 16:12:06 +1000

    Refactor daily goal tabs to have dynamic height per tab

--- src/components/DailyGoalTabs/DailyGoalTabs.tsx
+++ src/components/DailyGoalTabs/DailyGoalTabs.tsx
@@ -1,36 +1,45 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-import { Layout, Tab, TabView, TabViewElement } from '@ui-kitten/components';
+import { Layout, Tab, TabView, TabViewProps } from '@ui-kitten/components';
 import useDailyTasks from '../../hooks/useDailyTasks/useDailyTasks';
 import DailyGoalCard from '../DailyGoalCard/DailyGoalCard';
 
-const DailyGoalTabs = (): TabViewElement => {
+const DailyGoalTabs = (props: TabViewProps): React.ReactElement => {
   const [selectedIndex, setSelectedIndex] = React.useState<number>(0);
   const { allTasks, inProgressTasks, completedTasks } = useDailyTasks();
 
   return (
-    <TabView
-      selectedIndex={selectedIndex}
-      onSelect={index => setSelectedIndex(index)}
-      style={styles.tabViewContainer}
-      swipeEnabled={false}
-    >
-      <Tab title="All">
-        <Layout style={styles.tabContainer}>
-          {allTasks.map(task => <DailyGoalCard key={task.project.id} task={task} />)}
-        </Layout>
-      </Tab>
-      <Tab title="To Do">
-        <Layout style={styles.tabContainer}>
-          {inProgressTasks.map(task => <DailyGoalCard key={task.project.id} task={task} />)}
-        </Layout>
-      </Tab>
-      <Tab title="Completed">
-        <Layout style={styles.tabContainer}>
-          {completedTasks.map(task => <DailyGoalCard key={task.project.id} task={task} />)}
-        </Layout>
-      </Tab>
-    </TabView>
+    <>
+      <TabView
+        {...props}
+        selectedIndex={selectedIndex}
+        onSelect={index => setSelectedIndex(index)}
+        style={styles.tabViewContainer}
+      >
+        {/* Tabs children intentionally empty to avoid tab static height limitation */}
+        <Tab title="All"><React.Fragment /></Tab>
+        <Tab title="To Do"><React.Fragment /></Tab>
+        <Tab title="Completed"><React.Fragment /></Tab>
+      </TabView>
+      {/* Handle render tab content manually */}
+      <Layout style={styles.tabViewContainer}>
+        {selectedIndex === 0 &&
+          <Layout style={styles.tabContainer}>
+            {allTasks.map(task => <DailyGoalCard key={task.project.id} task={task} />)}
+          </Layout>
+        }
+        {selectedIndex === 1 &&
+          <Layout style={styles.tabContainer}>
+            {inProgressTasks.map(task => <DailyGoalCard key={task.project.id} task={task} />)}
+          </Layout>
+        }
+        {selectedIndex === 2 &&
+          <Layout style={styles.tabContainer}>
+            {completedTasks.map(task => <DailyGoalCard key={task.project.id} task={task} />)}
+          </Layout>
+        }
+      </Layout>
+    </>
   );
 };
 
@@ -42,7 +51,6 @@ const styles = StyleSheet.create({
   tabContainer: {
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 16,
     gap: 16,
   },
 });
