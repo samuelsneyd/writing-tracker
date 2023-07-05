baf6c04325edaf5c097a605cd584975cf656ce0a
Samuel <samuelsneyd@gmail.com>
2023-07-05 20:55:35 +1000

    Update jest config transform ignore rules

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,22 @@
-// import type { Config } from 'jest';
+import type { Config } from 'jest';
 
-export const config = {
+const config: Config = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|@ui-kitten|aws-amplify|react-native-gifted-charts)/)',
+    'node_modules/'
+    + '(?!('
+    + 'react-native'
+    + '|@react-native'
+    + '|@react-navigation'
+    + '|@ui-kitten'
+    + '|aws-amplify'
+    + '|aws-amplify-react-native'
+    + '|react-native-gifted-charts'
+    + '|react-native-linear-gradient'
+    + ')/)',
     'amplify/#current-cloud-backend',
   ],
-  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
-  // transform: {},
   setupFiles: ['<rootDir>/jest.setup.js'],
 };
 
-// export default config;
+export default config;
