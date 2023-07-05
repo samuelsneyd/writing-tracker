756150e5138d407dac38ac2a40f6e01674eb2a98
Samuel <samuelsneyd@gmail.com>
2023-07-05 20:43:07 +1000

    Add dev deps: redux-mock-store, ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,14 @@
-import type { Config } from 'jest';
+// import type { Config } from 'jest';
 
-const config: Config = {
+export const config = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|@ui-kitten|aws-amplify)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@ui-kitten|aws-amplify|react-native-gifted-charts)/)',
     'amplify/#current-cloud-backend',
   ],
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
+  // transform: {},
   setupFiles: ['<rootDir>/jest.setup.js'],
 };
 
-export default config;
+// export default config;
