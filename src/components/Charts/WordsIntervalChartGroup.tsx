26c078e772c348958632144ad03d1571764b3ed0
Samuel <samuelsneyd@gmail.com>
2023-07-09 10:22:30 +1000

    Refactor words interval chart group.
    Use UI Kitten tabs with lazy loading.
    
    Adjust styles.

--- src/components/Charts/WordsIntervalChartGroup.tsx
+++ src/components/Charts/WordsIntervalChartGroup.tsx
@@ -9,6 +9,7 @@ import { WordsIntervalYear } from './WordsIntervalYear';
 
 const WordsIntervalChartGroup = (props: TabViewProps) => {
   const [selectedIndex, setSelectedIndex] = React.useState<number>(1);
+  const shouldLoadComponent = (index: number): boolean => index === selectedIndex;
 
   return (
     <>
@@ -17,24 +18,19 @@ const WordsIntervalChartGroup = (props: TabViewProps) => {
         {...props}
         selectedIndex={selectedIndex}
         onSelect={index => setSelectedIndex(index)}
+        // Lazy loading
+        shouldLoadComponent={shouldLoadComponent}
         // Workaround for animation bug https://github.com/akveo/react-native-ui-kitten/issues/1234
         animationDuration={0}
         swipeEnabled={false}
         style={styles.tabViewContainer}
       >
-        {/* Tabs children intentionally empty to avoid tab static height limitation */}
-        <Tab title="Day"><React.Fragment /></Tab>
-        <Tab title="Week"><React.Fragment /></Tab>
-        <Tab title="Month"><React.Fragment /></Tab>
-        <Tab title="6 Month"><React.Fragment /></Tab>
-        <Tab title="Year"><React.Fragment /></Tab>
+        <Tab title="Day"><WordsIntervalDay showTitle={false} /></Tab>
+        <Tab title="Week"><WordsIntervalWeek showTitle={false} /></Tab>
+        <Tab title="Month"><WordsIntervalMonth showTitle={false} /></Tab>
+        <Tab title="6 Month"><WordsInterval6Month showTitle={false} /></Tab>
+        <Tab title="Year"><WordsIntervalYear showTitle={false} /></Tab>
       </TabView>
-      {/* Handle render tab content manually */}
-      {selectedIndex === 0 && <WordsIntervalDay showTitle={false} />}
-      {selectedIndex === 1 && <WordsIntervalWeek showTitle={false} />}
-      {selectedIndex === 2 && <WordsIntervalMonth showTitle={false} />}
-      {selectedIndex === 3 && <WordsInterval6Month showTitle={false} />}
-      {selectedIndex === 4 && <WordsIntervalYear showTitle={false} />}
     </>
   );
 };
@@ -43,11 +39,8 @@ const styles = StyleSheet.create({
   tabViewContainer: {
     flex: 1,
     width: '100%',
-  },
-  tabContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 16,
+    rowGap: 16,
+    paddingBottom: 8,
   },
 });
 
