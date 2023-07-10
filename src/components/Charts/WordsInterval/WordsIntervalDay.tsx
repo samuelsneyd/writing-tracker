ebc4e4906cba783a21a15a15dfd2216e696ea376
Samuel <samuelsneyd@gmail.com>
2023-07-10 11:24:05 +1000

    Memoize words interval day chart

--- src/components/Charts/WordsInterval/WordsIntervalDay.tsx
+++ src/components/Charts/WordsInterval/WordsIntervalDay.tsx
@@ -35,61 +35,73 @@ export const WordsIntervalDay = (props: ChartProps): React.ReactElement => {
   const reduxSessions = useAppSelector(state => state.sessions);
   const today = new Date();
   const [interval, setInterval] = React.useState<Interval>({
-    start: startOfDay(today),
-    end: endOfDay(today),
+    start: startOfDay(today).getTime(),
+    end: endOfDay(today).getTime(),
   });
-  const allDatesInInterval = eachDayOfInterval(interval).map(date => date.toISOString());
-  const { allTasks } = useDailyTasks(interval.start as Date);
+  const allDatesInInterval = React.useMemo(
+    () => eachDayOfInterval(interval).map(date => date.toISOString()),
+    [interval.start, interval.end],
+  );
+  const { allTasks } = useDailyTasks(interval.start);
   const dailyGoal = _.sumBy(allTasks, 'wordsToDo');
 
   // Sum words of all projects, grouped by day of the week
-  const barData = _(reduxSessions)
-    .filter(session => isWithinInterval(new Date(session.date), interval))
-    .map(session => ({
-      value: session.words,
-      day: startOfDay(new Date(session.date)).toISOString(),
-    }))
-    .groupBy('day')
-    .mapValues(group => _.sumBy(group, 'value'))
-    .defaults(_.zipObject(allDatesInInterval, Array(allDatesInInterval.length).fill(0)))
-    .map((value, day): BarDataItemType => {
-      const dayDate = new Date(day);
-      const dayIndex = (getDay(dayDate) + 6) % 7;
-      const label = 'Actual';
-      return ({
-        day,
-        dayIndex,
-        value,
-        label,
-        labelComponent: () => renderLabel(label),
-      });
-    })
-    // Sort chronologically
-    .sortBy('day')
-    .map((item): BarDataItemType => ({
-      ...item,
-      ...getSteppedColors(item, theme, dailyGoal),
-    }))
-    // Add the daily target
-    .push({
-      value: dailyGoal,
-      label: 'Target',
-      labelComponent: () => renderLabel('Target'),
-      day: interval.start,
-      frontColor: theme['color-success-500'],
-      gradientColor: theme['color-success-300'],
-      showGradient: true,
-    })
-    .value();
+  const barData = React.useMemo(
+    () => _(reduxSessions)
+      .filter(session => isWithinInterval(new Date(session.date), interval))
+      .map(session => ({
+        value: session.words,
+        day: startOfDay(new Date(session.date)).toISOString(),
+      }))
+      .groupBy('day')
+      .mapValues(group => _.sumBy(group, 'value'))
+      .defaults(_.zipObject(allDatesInInterval, Array(allDatesInInterval.length).fill(0)))
+      .map((value, day): BarDataItemType => {
+        const dayDate = new Date(day);
+        const dayIndex = (getDay(dayDate) + 6) % 7;
+        const label = 'Actual';
+        return ({
+          day,
+          dayIndex,
+          value,
+          label,
+          labelComponent: () => renderLabel(label),
+        });
+      })
+      // Sort chronologically
+      .sortBy('day')
+      .map((item): BarDataItemType => ({
+        ...item,
+        ...getSteppedColors(item, theme, dailyGoal),
+      }))
+      // Add the daily target
+      .push({
+        value: dailyGoal,
+        label: 'Target',
+        labelComponent: () => renderLabel('Target'),
+        day: interval.start,
+        frontColor: theme['color-success-500'],
+        gradientColor: theme['color-success-300'],
+        showGradient: true,
+      })
+      .value(),
+    [reduxSessions, interval.start, interval.end, allDatesInInterval, theme],
+  );
 
   // Average per day during current interval
-  const total = Math.round(_(barData).filter(data => data.value && data.label === 'Actual').sumBy('value')) || 0;
+  const total = React.useMemo(
+    () => Math.round(_(barData).filter(data => data.value && data.label === 'Actual').sumBy('value')) || 0,
+    [barData],
+  );
 
   // Custom logic for max value to display smaller daily targets < 1000 words as larger bars
-  const maxValue = dailyGoal < 1000
-    ? getMaxYAxisValue(barData, 100, 100)
-    : getMaxYAxisValue(barData);
-  const yAxisLabelTexts = getYAxisLabelTexts(maxValue);
+  const maxValue = React.useMemo(
+    () => dailyGoal < 1000
+      ? getMaxYAxisValue(barData, 100, 100)
+      : getMaxYAxisValue(barData),
+    [barData, dailyGoal],
+  );
+  const yAxisLabelTexts = React.useMemo(() => getYAxisLabelTexts(maxValue), [maxValue]);
 
   return (
     <>
@@ -101,14 +113,14 @@ export const WordsIntervalDay = (props: ChartProps): React.ReactElement => {
         intervalText={formatInterval(interval)}
         onBackButtonPress={() => {
           setInterval({
-            start: sub(interval.start, { days: 1 }),
-            end: sub(interval.end, { days: 1 }),
+            start: sub(interval.start, { days: 1 }).getTime(),
+            end: sub(interval.end, { days: 1 }).getTime(),
           });
         }}
         onForwardButtonPress={() => {
           setInterval({
-            start: add(interval.start, { days: 1 }),
-            end: add(interval.end, { days: 1 }),
+            start: add(interval.start, { days: 1 }).getTime(),
+            end: add(interval.end, { days: 1 }).getTime(),
           });
         }}
         forwardButtonDisabled={isWithinInterval(today, interval)}
